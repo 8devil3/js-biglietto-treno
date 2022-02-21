a480c71cb241ba905cc0598406b4667be7ef585c
8devil3 <8dev.il3@gmail.com>
2022-02-21 15:09:46 +0100

    js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,23 @@ let importo;
 let output = document.getElementById("importoOutput");
 
 
+// prompt
+distanza = prompt("Quanti chilometri vuoi percorrere?", "Inserisci un numero");
+eta = prompt("Quanti anni hai?", "Inserisci un numero");
+
+
+// condizioni
+if (eta < 18) {
+    importo = Math.round(((((prezzo * distanza) - (prezzo * distanza * sconto_1))) + Number.EPSILON) * 100) / 100;
+    
+} else if (eta > 65) {
+    importo = Math.round(((((prezzo * distanza) - (prezzo * distanza * sconto_2))) + Number.EPSILON) * 100) / 100;
+} else {
+    importo = Math.round((((prezzo * distanza)) + Number.EPSILON) * 100) / 100;
+}
+
+importo = importo.toFixed(2).toString();
+importo = importo.replace(".",",");
+
+// output
+output.innerHTML = "<strong>" + importo + "</strong>";
